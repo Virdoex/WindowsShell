5ad790b94193c2cd77ab730501327e24c78d3539
Deepak Dhiman <mpd90686@gmail.com>
2018-02-13 05:15:12 +0000

    Update server_tcp.py

--- server_tcp.py
+++ server_tcp.py
@@ -5,7 +5,7 @@ import sys
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 
-server.bind(("192.168.100.150",9000))
+server.bind(("your ip",9000))
 server.listen(1)
 while(1):
 	client,addr = server.accept()
