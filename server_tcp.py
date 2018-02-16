d0c46b55cfba5c0594c0480a42fbf5ae56e46917
Deepak Dhiman <mpd90686@gmail.com>
2018-02-16 04:44:35 +0000

    Update server_tcp.py

--- server_tcp.py
+++ server_tcp.py
@@ -7,17 +7,16 @@ server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 
 server.bind(("your ip",9000))
 server.listen(1)
-while(1):
-	client,addr = server.accept()
-	print "Connecting with ",addr
-	command = raw_input("shell>")
-	if 'terminate' in command:
-		client.send('terminate')
-		break
-	else:
-		client.send(command)
-		data = client.recv(2048)
-		print data
+client,addr = server.accept()
+print "Connecting with ",addr
+command = raw_input("shell>")
+if 'terminate' in command:
+	client.send('terminate')
+	break
+else:
+	client.send(command)
+	data = client.recv(2048)
+	print data
 		
 		
 	
